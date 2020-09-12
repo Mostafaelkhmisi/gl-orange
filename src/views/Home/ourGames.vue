6e45ff0e0202b24614175776d10d9482f9e1ecff
Mostafa <mostafaelkhmisi@hotmail.com>
2020-09-12 18:45:00 +0200

    t

--- src/views/Home/ourGames.vue
+++ src/views/Home/ourGames.vue
@@ -3,11 +3,85 @@
   <div class="row justify-content-center">
     <div class="ourGamesTitle"> <h2> <div class="title-orangeBox"></div>  <span>OUR</span> GAMES <div class="title-orangeBox"></div></h2> </div>
   </div>
+
+    <VueSlickCarousel v-bind="sliderSettings">
+        <template v-slot:prevArrow>
+            <i class="fas fa-chevron-left" id="gallery-prev"></i>
+        </template>
+            <div class="home-gallery-glider">
+                  <img src="@assets/imgs/backgrounds/SHbg.jpg" draggable="false" alt="">
+            </div>
+            <div class="home-gallery-glider">
+                  <img src="@assets/imgs/backgrounds/SHbg.jpg" draggable="false" alt="">
+            </div>
+                        <div class="home-gallery-glider">
+                  <img src="@assets/imgs/backgrounds/SHbg.jpg" draggable="false" alt="">
+            </div>
+                        <div class="home-gallery-glider">
+                  <img src="@assets/imgs/backgrounds/SHbg.jpg" draggable="false" alt="">
+            </div>
+        <template v-slot:nextArrow>
+            <i class="fas fa-chevron-right" id="gallery-next"></i>
+        </template>
+    </VueSlickCarousel>
 </div>
 </template>
 <script>
-export default {}
+import VueSlickCarousel from 'vue-slick-carousel'
+import 'vue-slick-carousel/dist/vue-slick-carousel.css'
+export default {
+  components: { VueSlickCarousel },
+  data () {
+    return {
+      sliderSettings: {
+        infinite: true,
+        speed: 500,
+        draggable: false,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [
+          {
+            breakpoint: 1400,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3
+            }
+          },
+          {
+            breakpoint: 992,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          }
+        ]
+      }
+    }
+  }
+}
 </script>
 
 <style lang="scss">
+  #gallery-next, #gallery-prev {
+    position: absolute;
+    z-index: 5;
+    top: 50%;
+    transform: translateY(-50%);
+    cursor: pointer;
+    width: 50px;
+    height: 50px;
+    color: #ccc;
+    font-size: 2rem;
+    transition: all .5s ease-in-out;
+      &:hover {
+        color: #fff;
+      }
+  }
+  #gallery-prev {
+    left: 0;
+  }
+  #gallery-next {
+    right: 0;
+    text-align: end;
+  }
 </style>
