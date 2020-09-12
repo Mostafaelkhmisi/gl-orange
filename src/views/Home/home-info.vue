90727a4cfd6601a2be14bf5318b6874f6a0185f7
Mostafa <mostafaelkhmisi@hotmail.com>
2020-09-12 12:04:51 +0200

    initial

--- src/views/Home/home-info.vue
+++ src/views/Home/home-info.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="homeInfo">
-    <img class="infoBackground-img" src="@assets/imgs/backgrounds/SHbg.jpg" width="100%">
+    <!-- <img class="infoBackground-img" src="@assets/imgs/backgrounds/SHbg.jpg" width="100%"> -->
     <div class="scrollTo-Padding">
         <div class="scrollToContainer">
             <div class="scrolltoShow"><i class="fas fa-arrow-down"></i></div>
@@ -19,9 +19,9 @@
                             <div class="col-6"> <img src="@assets/imgs/client/Orange.svg" width="80%" /></div>
                             <div class="col-6"> <img src="@assets/imgs/client/Orange.svg" width="80%" /> </div>
                             <div class="col-12">
-                                <p>For the first time in Egypt .. GO Games Internet packages for mobile gaming customers from the first pound of the day! GO Games</p>
-                                   <p> For 10 EGP per month, you will have 1000 MB length of the month and 300 MB for free at Wake ends</p>
-                                   <p> To subscribe, call 222 * 31 # # or through the My Orange app or log in to my account on the Orange website </p>
+                                    <p>For the first time in Egypt .. GO Games Internet packages for mobile gaming customers from the first pound of the day! GO Games</p>
+                                    <p> For 10 EGP per month, you will have 1000 MB length of the month and 300 MB for free at Wake ends</p>
+                                    <p> To subscribe, call 222 * 31 # # or through the My Orange app or log in to my account on the Orange website </p>
                                 <div class="orangeWebsite-link"> <p class="link-hover"> <i class="fas fa-chevron-left third-arrow"></i>  <i class="fas fa-chevron-left second-arrow"></i> <i class="fas fa-chevron-left"></i> <a href="#"> Visit our website </a> <i class="fas fa-chevron-right"></i> <i class="fas fa-chevron-right second-arrow"></i> <i class="fas fa-chevron-right third-arrow"></i>  </p> </div>
                             </div>
                         </div>
