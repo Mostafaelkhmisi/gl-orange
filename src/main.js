1c051aeb40b9961f114de6e21971fb85f6e733a4
mostafa <mostafaelkhmisi@hotmail.com>
2020-10-07 13:07:13 +0200

    AOS animation once

--- src/main.js
+++ src/main.js
@@ -1,33 +1,35 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
 
-import fontawesome from '@/assets/fontAwesome/css/all.min.css'
-import generalStyle from '@/assets/style/general.scss'
+import fontawesome from '@/assets/fontAwesome/css/all.min.css';
+import generalStyle from '@/assets/style/general.scss';
 
-import AOS from 'aos'
-import 'aos/dist/aos.css'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
-import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
-import Vuelidate from 'vuelidate'
-Vue.use(Vuelidate)
+import Vuelidate from 'vuelidate';
+Vue.use(Vuelidate);
 
-Vue.use(BootstrapVue)
-Vue.use(BootstrapVueIcons)
-Vue.use(fontawesome)
-Vue.use(generalStyle)
+Vue.use(BootstrapVue);
+Vue.use(BootstrapVueIcons);
+Vue.use(fontawesome);
+Vue.use(generalStyle);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
   created () {
-    AOS.init()
+    AOS.init({
+      once: true // whether animation should happen only once - while scrolling down
+    });
   },
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
