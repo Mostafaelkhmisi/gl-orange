9a3d819e8ea63ec1dec5709480ff1f13888e6598
mostafa <mostafaelkhmisi@hotmail.com>
2020-10-07 15:13:55 +0200

    styled scrollbar

--- src/App.vue
+++ src/App.vue
@@ -23,4 +23,16 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
+::-webkit-scrollbar{
+  width: 7px;
+}
+::-webkit-scrollbar-track{
+  //border: 4px solid orange;
+  //box-shadow: inset 0 0 2.5px 2px rgba(0, 0, 0 ,0.5);
+  background-color: black;
+}
+::-webkit-scrollbar-thumb{
+  background: linear-gradient(45deg,#ff6b0969, #FF6600CC);
+  border-radius: 5px;
+}
 </style>
